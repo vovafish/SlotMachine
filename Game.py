b8c101916dd57e45a34238ee01887b43154c67df
Vladimirs <vladimirribakov123@mail.ru>
2024-05-24 22:14:57 +0100

    created function that takes all rows,columns and symbols for them and assign them to each slot and when that particular symbol got assign it removes from all the symbols that left

--- Game.py
+++ Game.py
@@ -1,6 +1,39 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():  #to get the key and the value
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
+    for _ in range(cols):
+        column = []
+        current_sybmols = all_symbols[:] #to copy a list
+        for _ in range(rows):
+            value = random.choice(current_sybmols)
+            current_sybmols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 def deposit():
     while True:
@@ -31,6 +64,7 @@ def get_number_of_line():
 
     return lines
 
+
 def get_bet():
     while True:
         amount = input("What is your bet on each line? $")
@@ -45,10 +79,20 @@ def get_bet():
 
     return amount
 
+
 def main():
     balance = deposit()
     lines = get_number_of_line()
-    bet = get_bet()
-    print(balance, lines, bet)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"Your balance doesnt have enough money, you current balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
+
 
 main()

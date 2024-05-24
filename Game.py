537bd88bf44f6570c588a0d50fbd52c3737b95cf
Vladimirs <vladimirribakov123@mail.ru>
2024-05-24 21:21:05 +0100

    deposit function, keep asking user what amount he want to deposit until the number is valid

--- Game.py
+++ Game.py
@@ -1,1 +1,14 @@
-#slot machine
+
+def deposit():
+    while True:
+        amount = input("What is your deposit $? ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number: ")
+
+    return amount
